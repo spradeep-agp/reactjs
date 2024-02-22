ed28b1dd82e07681c54fd5a692081d47b62cd0e7
spradeep-agp <spradeep.agp@gmail.com>
2024-02-22 10:45:33 +0530

    Printing started

--- FullCourse1/p2-reactdemo/src/App.js
+++ FullCourse1/p2-reactdemo/src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import PrintMe from './p1-intro/f1-print';
 
 function App() {
   return (
@@ -7,6 +8,8 @@ function App() {
       <header className="App-header">
         React App
       </header>
+      <hr/>
+      <PrintMe/>
     </div>
   );
 }
---
+++ FullCourse1/p2-reactdemo/src/p1-intro/f1-print.js
@@ -0,0 +1,16 @@
+function PrintMe(){
+    const name = "MyReactApp";
+
+    return(
+        <div>
+            <p>name</p>
+            <p>{name}</p>
+            <p>{"name"}</p>
+            <p>{"Welcome "+name}</p>
+            <p>{[1,3,5]}</p>
+            {/* <p>{{roll:101,name:'raj'}}</p> */}
+        </div>
+    )
+}
+
+export default PrintMe;
